9da160f6e7c0d0319b9fc0175c9e460463f8de90
pablogm9 <60946030+pablogm9@users.noreply.github.com>
2020-03-09 16:00:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ reference_data,reference_headers,reference_descriptions = Read.get_data('ref_dat
 
 # --- Compute thrust ---
 
-
 # Pressure altitude [m]
 h_p_index = np.where(reference_headers=='Dadc1_alt [ft]')[0].flat[0]
 h_p = np.array(reference_data[reference_headers[h_p_index]])*0.3048
@@ -60,7 +59,20 @@ right_fuelflow_index = np.where(reference_headers=='rh_engine_FMF [lbs/hr]')[0].
 right_fuelflow = np.array(reference_data[reference_headers[right_fuelflow_index]])
 
 
-# Temperature difference 
+# Static temperature converted to [K]
+T_static_index = np.where(reference_headers=='Dadc1_sat [deg C]')[0].flat[0]
+T_static = np.array(reference_data[reference_headers[T_static_index]])+273.15
+
+# ISA temperature [K]
+T0 = 273.15+15 #[K]
+a = -6.5/1000 #[deg C / m]
+T_ISA = h_p*a + T0
+
+# Temperature difference
+delta_T = T_static - T_ISA
+
+# Write parameters to .mat file
+
 
 # Call thrust.exe 
 
