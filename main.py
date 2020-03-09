d95e36050f41d8df56cea288241e2d20c430f11f
pablogm9 <60946030+pablogm9@users.noreply.github.com>
2020-03-09 17:04:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import Read
+from Thrust import thrust
 
 
 
@@ -31,51 +32,6 @@ reference_data,reference_headers,reference_descriptions = Read.get_data('ref_dat
 # ---------------- ANALYSIS OF STATIONARY MEASUREMENT DATA ----------------
  
 
-# --- Compute weight ---
-
-
-
-# --- Compute C_L ---
-
-
-
-# --- Compute thrust ---
-
-# Pressure altitude [m]
-h_p_index = np.where(reference_headers=='Dadc1_alt [ft]')[0].flat[0]
-h_p = np.array(reference_data[reference_headers[h_p_index]])*0.3048
-
-# Mach Number [-]
-M_index = np.where(reference_headers=='Dadc1_mach [mach]')[0].flat[0]
-M = np.array(reference_data[reference_headers[M_index]])
-
-# Left engine fuel flow [kg/s]
-left_fuelflow_index = np.where(reference_headers=='lh_engine_FMF [lbs/hr]')[0].flat[0]
-left_fuelflow = np.array(reference_data[reference_headers[left_fuelflow_index]])
-
-# Right engine fuel flow [kg/s]
-right_fuelflow_index = np.where(reference_headers=='rh_engine_FMF [lbs/hr]')[0].flat[0]
-right_fuelflow = np.array(reference_data[reference_headers[right_fuelflow_index]])
-
-# Static temperature converted to [K]
-T_static_index = np.where(reference_headers=='Dadc1_sat [deg C]')[0].flat[0]
-T_static = np.array(reference_data[reference_headers[T_static_index]])+273.15
-
-# ISA temperature [K]
-T0 = 273.15+15 #[K]
-a = -6.5/1000 #[deg C / m]
-T_ISA = h_p*a + T0
-
-# Temperature difference
-delta_T = T_static - T_ISA
-
-# Write parameters to .mat file
-
-
-# Call thrust.exe 
-
-
-# --- Compute C_D ---
 
 
 
