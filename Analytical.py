1c6f05ef156d0a74656733a19e66a49888744684
pietrosilvagni <49947408+pietrosilvagni@users.noreply.github.com>
2020-03-10 17:05:43 +0100

    Changing spaces

--- Analytical.py
+++ Analytical.py
@@ -1,19 +1,61 @@
 from Resources.Cit_par import *
-from math import *
+from math import pi, e, log, sqrt
 
-Cl=1
-import numpy as np
+#empy lists to fill with values
+lambdas     = []
+lambdas_real = []
+lambdas_imag = []
+t_half      = []
+taus        = []
+periods     = []
+no_per_half = []
+log_decr    = []
+damp_ratio  = []
+
+
+#========================SYMMETRIC-MOTIONS=============================
+
+#--------------------------SHORT-PERIOD----------------------------
+A_sp = 2 * muc * KY2**2 * (2 * muc - CZadot)
+B_sp = - 2 * muc * KY2**2 * CZa - (2 * muc + CZq) * Cmadot - (2 * muc - CZadot) * Cmq
+C_sp = CZa * Cmq - (2 * muc + CZq) * Cma
+
+Eig_val_short_period_real = (-B_sp) / (2 * A_sp)
+Eig_val_short_period_imag = sqrt(4 * A_sp * C_sp - B_sp**2) / (2 * A_sp)
+Eig_val_short_period_1 = complex(Eig_val_short_period_real, Eig_val_short_period_imag)
+Eig_val_short_period_2 = complex(Eig_val_short_period_real, -1*Eig_val_short_period_imag)
+lambdas.append(Eig_val_short_period_1)
+lambdas.append(Eig_val_short_period_2)
+
+# #----------------------Characteristics------------------------------------------------------------
+# thalf = log(0.5, e) * c / V / sqrt(Eig_val_short_period_real**2 + Eig_val_short_period_imag**2)
+# period = 2 * pi * c / (Eig_val_short_period_imag * V)
+# log_decrement = Eig_val_short_period_real * V / c * period
+# damp_ratio = - Eig_val_short_period_real / sqrt(Eig_val_short_period_real**2 + Eig_val_short_period_imag**2)
+
+#--------------------------PHUGOID---------------------------------------------------------
+A_ph = 2 * muc * (CZa * Cmq - 2 * muc * Cma)
+B_ph = 2 * muc * (CXu * Cma - Cmu * CXa) + Cmq * (CZu * CXa - CXu * CZa)
+C_ph = CZ0 * (Cmu * CZa - CZu * Cma)
+
+Eig_val_phugoid_real = (-B_ph) / (2 * A_ph)
+Eig_val_phugoid_img = sqrt(4 * A_ph * C_ph - B_ph**2) / (2 * A_ph)
+Eig_val_phugoid_1 = complex(Eig_val_phugoid_real, Eig_val_phugoid_img)
+Eig_val_phugoid_2 = complex(Eig_val_phugoid_real, -1*Eig_val_phugoid_img)
+lambdas.append(Eig_val_phugoid_1)
+lambdas.append(Eig_val_phugoid_2)
+
+#-----------------Characteristics----------------------------------------------------------
+thalf = log(0.5, e) * c / V / sqrt(Eig_val_phugoid_real**2 + Eig_val_phugoid_img**2)
+period = 2 * pi * c / (Eig_val_phugoid_imag * V)
+log_decrement = Eig_val_phugoid_real * V / c * period
+damp_ratio = - Eig_val_phugoid_real / sqrt(Eig_val_phugoid_real**2 + Eig_val_phugoid_imag**2)
 
 
 
-#Calculation of eigenvectors and values for symmetric and asymmetric
-#eigenmotions
+#============================ASYMMETRIC- MOTIONS========================================
 
-#List of available parameters for aymmetric flight
-lambdas=[]
-t_half=[]
-taus= []
-periods = []
+
 
 #--------------------Aperiodic roll----------------------
 lambda_aroll = Clp/(4*mub*KX2)
@@ -38,17 +80,9 @@ def thalf(x):
 print(lambdas)
 def tau(y):
     return -1*c/(Vh_V*y)
-def per(z):
-    return 2*pi*c/()
 for i in lambdas:
     t_half.append(thalf(i))
 for i in lambdas:
     taus.append(tau(i))
 print('Times to reach half amplitude=',t_half)
 print('The time constants=', taus)
-
-
-
-
-
-
