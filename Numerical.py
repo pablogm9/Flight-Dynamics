e396fe21639c4434c8c9284097ca35a7c37b8265
fed12345 <f.magri12@gmail.com>
2020-03-13 16:39:28 +0100

    Readme Updated

--- Numerical.py
+++ Numerical.py
@@ -2,6 +2,17 @@ from Resources.Cit_par import *
 import scipy as np
 import control.matlab as ml
 import matplotlib.pyplot as plt
+import control as ctr
+import Read
+####GETTING DATA FOR INPUTS###########
+
+parameters =  np.array(['vane_AOA', 'elevator_dte', 'column_fe', 'lh_engine_FMF', 'rh_engine_FMF', 'lh_engine_itt', 'rh_engine_itt', 'lh_engine_OP', 'rh_engine_OP', 'lh_engine_fan_N1', 'lh_engine_turbine_N2', 'rh_engine_fan_N1', 'rh_engine_turbine_N2',
+                'lh_engine_FU', 'rh_engine_FU', 'delta_a', 'delta_e', 'delta_r', 'Gps_date', 'Gps_utcSec', 'Ahrs1_Roll', 'Ahrs1_Pitch', 'Fms1_trueHeading', 'Gps_lat', 'Gps_long', 'Ahrs1_bRollRate', 'Ahrs1_bPitchRate', 'Ahrs1_bYawRate',
+                'Ahrs1_bLongAcc', 'Ahrs1_bLatAcc', 'Ahrs1_bNormAcc', 'Ahrs1_aHdgAcc', 'Ahrs1_xHdgAcc', 'Ahrs1_VertAcc', 'Dadc1_sat', 'Dadc1_tat', 'Dadc1_alt', 'Dadc1_bcAlt', 'Dadc1_bcAltMb', 'Dadc1_mach', 'Dadc1_cas', 'Dadc1_tas',
+                'Dadc1_altRate', 'measurement_running', 'measurement_n_rdy', 'display_graph_state', 'display_active_screen', 'time'])
+
+reference_data,reference_headers,reference_descriptions = Read.get_data('ref_data')
+flight_data,flight_headers,flight_descriptions = Read.get_data('testflight2')
 
 
 ########## ASYMMETRIC EQUATIONS OF MOTION IN STATE-SPACE FORM ##########
@@ -81,7 +92,56 @@ sys2 = ml.ss(A2, B2, C2, D2)
 
 #ml.pzmap(sys1) #eigenvalues plot
 #ml.pzmap(sys2)
+
+
+########### Simulation of Eigen Motions########
+dt= 0.1
+t = np.arange(0 , 100, dt)
+
+#Short Period
+
+t_delta = int(10/dt) #time the elevator is deflected
+s_def = -0.031 #angle of deflection[rad]
+s_e = np.ones((t_delta,1))
+s_en = np.array([i*s_def for i in s_e])
+zeros = np.zeros((900,1))
+s_e = np.vstack((s_en,zeros))
+
+#y , T, x = ml.lsim(sys2, s_e, t)
+
+
+#plt.plot(T, y[:,1])
+#plt.show()
+
+# Phugoid
+t = np.arange(0 , 7000, dt)
+
+#getting data
+delta_e_index = np.where(parameters=='delta_e')[0].flat[0]
+
+flight_delta_e = np.array(flight_data[flight_headers[delta_e_index]])
+value = flight_delta_e[32280:33400]
+s_e_phu = np.array([flight_delta_e[32280:33400]])
+
+s_e_phun = np.array([i*0.017455 for i in s_e_phu])
+missing = len(t) - len(value)
+zeros_phu = np.zeros((1,missing))
+s_e_phu_n = np.hstack((s_e_phun,zeros_phu))
+s_e_phu_n = np.transpose(s_e_phu_n)
+
+y , T, x = ml.lsim(sys2, s_e_phu_n, t)
+plt.plot(T[1:300], y[:,0][1:300])
+plt.show()
+
+####Dutch Roll###########
+
+u = [-0.130899,0] #deflection
+
+#T, y = ctr.impulse_response(sys2,t)
+#plt.plot(T[1:300], y[1][1:300])
+
 ########## Print Commands ##########
+
 
 #print(e1, v2)
 
