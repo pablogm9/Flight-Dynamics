Numerical EOM fixed

Numerical.py
@@ -17,74 +17,85 @@ flight_data,flight_headers,flight_descriptions = Read.get_data('testflight2')
 
 ########## ASYMMETRIC EQUATIONS OF MOTION IN STATE-SPACE FORM ##########
 
-y = [(V0/b) * (CYb/(2 * mub)),
-     (V0/b) * (CL/(2 * mub)),
-     (V0/b) * (CYp/(2 * mub)),
-     (V0/b) * ((CYr - 4 * mub)/(2 * mub)),
-     (V0/b) * (CYda/(2 * mub)),
-     (V0/b) * (CYdr/(2 * mub))]
+P1 = np.array([[(CYb - 2 * mub) * (c/V0), 0, 0, 0],
+               [0, -(1/2), 0, 0],
+               [0, 0, - 4 * mub * KX2 * (c/V0), 4 * mub * KXZ * (c/V0)],
+               [Cnbdot * (c/V0), 0, 4 * mub * KXZ * (c/V0), - 4 * mub * KZ2 * (c/V0)]])
 
-l = [(V0/b) * ((Clb * KZ2 + Cnb * KXZ)/(4 * mub * (KX2 * KZ2 - KXZ**2))),
-     0,
-     (V0/b) * ((Clp * KZ2 + Cnp * KXZ)/(4 * mub * (KX2 * KZ2 - KXZ**2))),
-     (V0/b) * ((Clr * KZ2 + Cnr * KXZ)/(4 * mub * (KX2 * KZ2 - KXZ**2))),
-     (V0/b) * ((Clda * KZ2 + Cnda * KXZ)/(4 * mub * (KX2 * KZ2 - KXZ**2))),
-     (V0/b) * ((Cldr * KZ2 + Cndr * KXZ)/(4 * mub * (KX2 * KZ2 - KXZ**2)))]
+Q1 = np.array([[-CYb, -CL, -CYp, 4 * mub - CYr],
+               [0, 0, -1, 0],
+               [-Clb, 0, -Clp, -Clr],
+               [-Cnb, 0, -Cnp, -Cnr]])
 
-n = [(V0/b) * ((Clb * KXZ + Cnb * KX2)/(4 * mub * (KX2 * KZ2 - KXZ**2))),
-     0,
-     (V0/b) * ((Clp * KXZ + Cnp * KX2)/(4 * mub * (KX2 * KZ2 - KXZ**2))),
-     (V0/b) * ((Clr * KXZ + Cnr * KX2)/(4 * mub * (KX2 * KZ2 - KXZ**2))),
-     (V0/b) * ((Clda * KXZ + Cnda * KX2)/(4 * mub * (KX2 * KZ2 - KXZ**2))),
-     (V0/b) * ((Cldr * KXZ + Cndr * KX2)/(4 * mub * (KX2 * KZ2 - KXZ**2)))]
+P11 = np.linalg.inv(P1)
 
-A1 = np.array([[y[0], y[1], y[2], y[3]], [0, 0, 2 * (V0/b), 0], [l[0], l[1], l[2], l[3]], [n[0], n[1], n[2], n[3]]])
+R1 = np.array([[-CYda, -CYdr],
+               [0, 0],
+               [-Clda, -Cldr],
+               [-Cnda, -Cndr]])
 
-B1 = np.array([[0, y[5]], [0, 0], [l[4], l[5]], [n[4], n[5]]])
+A1 = np.matmul(P11, Q1)
+
+B1 = np.matmul(P11, R1)
 
 C1 = np.identity(4)
 
-D1 = np.array([[0,0], [0,0], [0,0], [0,0]])
+D1 = np.array([[0, 0], [0, 0], [0, 0], [0, 0]])
 
-
-
+print(A1)
+print(B1)
+print(C1)
+print(D1)
 
 ########## SYMMETRIC EQUATIONS OF MOTION IN STATE-SPACE FORM ##########
 
-x = [(V0/c) * (CXu/(2 * muc)),
-     (V0/c) * (CXa/(2 * muc)),
-     (V0/c) * (CZ0/(2 * muc)),
-     (V0/c) * (CXq/(2 * muc)),
-     (V0/c) * (CXde/(2 * muc))]
+P2 = np.array([[- 2 * muc * (c/V0), 0, 0, 0],
+              [0, (CZadot - 2 * muc) * (c/V0), 0, 0],
+              [0, 0, - (c/V0), 0],
+              [0, Cmadot * (c/V0), 0, - 2 * muc * KY2 * (c/V0)]])
 
-z = [(V0/c) * (CZu/(2 * muc - CZadot)),
-     (V0/c) * (CZa/(2 * muc - CZadot)),
-     (V0/c) * (CX0/(2 * muc - CZadot)),
-     (V0/c) * ((2 * muc + CZq)/(2 * muc - CZadot)),
-     (V0/c) * (CZde/(2 * muc - CZadot))]
+Q2 = np.array([[-CXu, -CXa, -CZ0, -CXq],
+               [-CZu, -CZa, CX0, -(CZadot + 2 * muc)],
+               [0, 0, 0, -1],
+               [-Cmu, -Cma, 0, -Cmq]])
 
-m = [(V0/c) * ((Cmu + CZu * ((Cmadot)/(2 * muc - CZadot)))/(2 * muc * KY2)),
-     (V0/c) * ((Cma + CZa * ((Cmadot)/(2 * muc - CZadot)))/(2 * muc * KY2)),
-     - (V0/c) * ((CX0 * ((Cmadot)/(2 * muc - CZadot)))/(2 * muc * KY2)),
-     (V0/c) * ((Cmq + Cmadot * ((2 * muc + CZq)/(2 * muc - CZadot)))/(2 * muc * KY2)),
-     (V0/c) * ((Cmde + CZde * ((Cmadot)/(2 * muc - CZadot)))/(2 * muc * KY2))]
+P21 = np.linalg.inv(P2)
 
-A2 = np.array([[x[0], x[1], x[2], 0], [z[0], z[1], z[2], z[3]], [0, 0, 0, V0/c], [m[0], m[1], m[2], m[3]]])
+R2 = np.array([[-CXde],
+               [-CZde],
+               [0],
+               [-Cmde]])
 
-B2 = np.array([[x[4]], [z[4]], [0], [m[4]]])
+
+#print(np.shape(P21))
+#print(np.shape(Q2))
+#print(np.shape(R2))
+
+
+A2 = np.matmul(P21, Q2)
+
+B2 = np.matmul(P21, R2)
 
 C2 = np.identity(4)
 
 D2 = np.array([[0], [0], [0], [0]])
 
+print(A2)
+print(B2)
+print(C2)
+print(D2)
+
 ########## Eigenvalues and Eigenvectors ##########
 
-[e2, v2] = np.linalg.eig(A2)
 [e1, v1] = np.linalg.eig(A1)
+[e2, v2] = np.linalg.eig(A2)
+
+#print(e1, v1)
+#print(e2, v2)
 
 ########## Continuous time-state-space Model ##########
 
-
+#t = np.arange(0 , 100, 1)
 
 sys1 = ml.ss(A1, B1, C1, D1)
 
@@ -97,22 +108,22 @@ sys2 = ml.ss(A2, B2, C2, D2)
 
 ########### Simulation of Eigen Motions########
 dt= 0.1
-t = np.arange(0 , 10, dt)
+t = np.arange(0 , 100, dt)
 
 #Short Period
 
-t_delta = int(0.1/dt) #time the elevator is deflected
+t_delta = int(10/dt) #time the elevator is deflected
 s_def = -0.031 #angle of deflection[rad]
 s_e = np.ones((t_delta,1))
 s_en = np.array([i*s_def for i in s_e])
-zeros = np.zeros((99,1))
+zeros = np.zeros((900,1))
 s_e = np.vstack((s_en,zeros))
 
-y , T, x = ml.lsim(sys2, s_e, t)
+#y , T, x = ml.lsim(sys2, s_e, t)
 
 
-plt.plot(T, y[:,3])
-plt.show()
+#plt.plot(T, y[:,1])
+#plt.show()
 
 # Phugoid
 t = np.arange(0 , 7000, dt)
@@ -130,15 +141,15 @@ zeros_phu = np.zeros((1,missing))
 s_e_phu_n = np.hstack((s_e_phun,zeros_phu))
 s_e_phu_n = np.transpose(s_e_phu_n)
 
-#y , T, x = ml.lsim(sys2, s_e_phu_n, t)
-#plt.plot(T[1:300], y[:,0][1:300])
-#plt.show()
+y , T, x = ml.lsim(sys2, s_e_phu_n, t)
+plt.plot(T[1:300], y[:,0][1:300])
+plt.show()
 
 ####Dutch Roll###########
 
 u = [-0.130899,0] #deflection
 
-T, y = ctr.impulse_response(sys2,t)
+#T, y = ctr.impulse_response(sys2,t)
 #plt.plot(T[1:300], y[1][1:300])
 
 ########## Print Commands ##########